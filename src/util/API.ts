0cb7961b9ad0aebc8f9406656fe101aa126a9b7e
ctcuff <dev.ctcuff@gmail.com>
2022-02-04 17:24:16 -0500

    Add package to load variables from .env

--- src/util/API.ts
+++ src/util/API.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { User } from '../types/API';
+import { DEBUG_API_URL, PROD_API_URL } from '@env';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:4565/api' : '/inventory/api';
+axios.defaults.baseURL = __DEV__ ? DEBUG_API_URL : PROD_API_URL;
 
 class API {
   static async login(nid: string, password: string): Promise<User> {
